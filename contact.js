d51293296bb858dd32df637a94ded8ac5951cb5b
risanjayy <risyadul@thelionparcel.com>
2023-12-15 13:42:19 +0700

    install validator name, email, noHp. Bagian 2

--- contact.js
+++ contact.js
@@ -1,6 +1,5 @@
-const { rejects } = require('assert');
 const fs = require('fs');
-const { resolve } = require('path');
+const validator = require('validator');
 
 // membuat folder data jika belum ada
 const dirPath = './data';
@@ -13,24 +12,33 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        })
-    })
-}
-
 const simpanContact = (nama, email, noHp) => {
     const contact = { nama, email, noHp };
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
     const contacts = JSON.parse(fileBuffer);
 
+    const duplicate = contacts.find((contact) => contact.nama === nama)
+    if (duplicate) {
+        console.log('Contact sudah terdaftar, gunakan nama lain!');
+        return false;
+    }
+
+    if (email) {
+        if (!validator.isEmail(email)) {
+            console.log('Email tidak valid..');
+            return false;
+        }
+    }
+
+    if (!validator.isMobilePhone(noHp, 'id-ID')) {
+        console.log('Nomor Hp tidak valid, tolong perbaiki...');
+        return false;
+    }
+
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
     console.log(`Terimakasih sudah memasukkan data`);
-    rl.close();
 }
 
-module.exports = { tulisPertanyaan, simpanContact };
+module.exports = { simpanContact };
