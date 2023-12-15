f8f8d2ba9c24485ff811664fa91b0d743f793cf0
risanjayy <risyadul@thelionparcel.com>
2023-12-15 08:25:00 +0700

    bagian 1

---
+++ contact.js
@@ -0,0 +1,42 @@
+const { rejects } = require('assert');
+const fs = require('fs');
+const { resolve } = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+// membuat folder data jika belum ada
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+
+const dataPath = './data/contacts.json';
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8');
+}
+
+const tulisPertanyaan = (pertanyaan) => {
+    return new Promise((resolve, reject) => {
+        rl.question(pertanyaan, (nama) => {
+            resolve(nama);
+        })
+    })
+}
+
+const simpanContact = (nama, email, noHp) => {
+    const contact = { nama, email, noHp };
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(fileBuffer);
+
+    contacts.push(contact);
+
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+    console.log(`Terimakasih sudah memasukkan data`);
+    rl.close();
+}
+
+module.exports = { tulisPertanyaan, simpanContact };
