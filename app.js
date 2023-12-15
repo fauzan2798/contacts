c559f44b4f2fe15bad5cb008d88429ae0c4d3a75
risanjayy <risyadul@thelionparcel.com>
2023-12-15 13:17:40 +0700

    install yargs

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const { tulisPertanyaan, simpanContact } = require('./contact');
+const yargs = require('yargs');
+const { simpanContact } = require('./contact');
 
-const main = async () => {
-    const nama = await tulisPertanyaan('Masukkan nama anda : ');
-    const email = await tulisPertanyaan('Masukkan email anda : ');
-    const noHp = await tulisPertanyaan('Masukkan no hp anda : ');
+yargs.command({
+    command: 'add',
+    describe: 'add new contact',
+    builder: {
+        name: {
+            describe: 'Full Name',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string',
+        },
+        phone: {
+            describe: 'Mobile Phone',
+            demandOption: 'true',
+            type: 'string',
+        }
+    },
+    handler(argv) {
+        simpanContact(argv.name, argv.email, argv.phone);
+    }
+})
 
-    simpanContact(nama, email, noHp);
-}
+yargs.parse();
 
-main();
+// const main = async () => {
+//     const nama = await tulisPertanyaan('Masukkan nama anda : ');
+//     const email = await tulisPertanyaan('Masukkan email anda : ');
+//     const noHp = await tulisPertanyaan('Masukkan no hp anda : ');
+
+//     simpanContact(nama, email, noHp);
+// }
+
+// main();
--- contact.js
+++ contact.js
@@ -1,12 +1,6 @@
 const { rejects } = require('assert');
 const fs = require('fs');
 const { resolve } = require('path');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
 
 // membuat folder data jika belum ada
 const dirPath = './data';
